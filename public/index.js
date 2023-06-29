c654589b159cbe7d94027158c2da3d264b1f7e41
Diego Velasquez <100886672+da-velasquezgomez@users.noreply.github.com>
2023-06-28 19:17:22 -0700

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,76 @@
+function getColor(stock) {
+  if (stock === "GME") {
+    return "rgba(61, 161, 61, 0.7)";
+  }
+  if (stock === "MSFT") {
+    return "rgba(209, 4, 25, 0.7)";
+  }
+  if (stock === "DIS") {
+    return "rgba(18, 4, 209, 0.7)";
+  }
+  if (stock === "BNTX") {
+    return "rgba(166, 43, 158, 0.7)";
+  }
+}
+
+async function main() {
+  const timeChartCanvas = document.querySelector("#time-chart");
+  const highestPriceChartCanvas = document.querySelector(
+    "#highest-price-chart"
+  );
+  const averagePriceChartCanvas = document.querySelector(
+    "#average-price-chart"
+  );
+  const response = await fetch(
+    `https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=bae8cbe131db4282ba413f7f86a51883`
+  );
+  const result = await response.json();
+
+  //console.log(result)
+  const { GME, MSFT, DIS, BNTX } = result;
+  const stocks = [GME, MSFT, DIS, BNTX];
+  console.log(stocks);
+
+  stocks.forEach((stock) => stock.values.reverse());
+
+  //Time Chart
+  new Chart(timeChartCanvas.getContext("2d"), {
+    type: "line",
+    data: {
+      labels: stocks[0].values.map((value) => value.datetime),
+      datasets: stocks.map((stock) => ({
+        label: stock.meta.symbol,
+        data: stock.values.map((value) => parseFloat(value.high)),
+        backgroundColor: getColor(stock.meta.symbol),
+        borderColor: getColor(stock.meta.symbol),
+      })),
+    },
+  });
+
+  //Highest Stock Price Chart
+  new Chart(highestPriceChartCanvas.getContext("2d"), {
+    type: "bar",
+    data: {
+      labels: stocks.map((stock) => stock.meta.symbol),
+      datasets: [
+        {
+          label: "Highest",
+          backgroundColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+          borderColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+          data: stocks.map((stock) => findLargest(stock.values)),
+        },
+      ],
+    },
+  });
+}
+
+function findLargest(values) {
+  var HighestValue1 = 0;
+
+  values.forEach((value) => {
+    if (parseFloat(value.high) > HighestValue1) HighestValue1 = value.high;
+  });
+  return HighestValue1;
+}
+
+main();
